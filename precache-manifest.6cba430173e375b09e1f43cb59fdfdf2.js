09152e4a6ed3fadaf095fb7dd79652ffa199a5a5
vvxforce <vvxforce@gmail.com>
2019-12-06 09:11:03 +0200

    Updates

---
+++ precache-manifest.6cba430173e375b09e1f43cb59fdfdf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9952b71eeb050d940e24a58ec1c3890f",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "5a777df46bfb4846d5c9",
+    "url": "/react_static-list-of-posts/static/css/main.28e8e4b5.chunk.css"
+  },
+  {
+    "revision": "3485fa9145fb43f72c73",
+    "url": "/react_static-list-of-posts/static/js/2.506750e1.chunk.js"
+  },
+  {
+    "revision": "5a777df46bfb4846d5c9",
+    "url": "/react_static-list-of-posts/static/js/main.949960ae.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
